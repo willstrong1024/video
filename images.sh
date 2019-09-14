d3f48d9bd9a003dcdf76302f37626cd74457cd38
William Strong <me@williamstrong.co.uk>
2019-09-14 17:22:08 +0100

    fixed bug where image has missing section

--- images.sh
+++ images.sh
@@ -4,6 +4,6 @@ mkdir -p /tmp/video/images
 
 i=0
 for post in /tmp/video/posts/post*; do
-	capture-website --element="#t3_$(cat ${post}/id)" --full-page "$(cat ${post}/url)" "/tmp/video/images/post${i}.png"
+	capture-website --element="#t3_$(cat ${post}/id)" --height=10000 "$(cat ${post}/url)" "/tmp/video/images/post${i}.png"
 	i=$((i + 1))
 done
